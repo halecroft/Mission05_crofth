dc3020775185bc2cbc4ca562270d00131d4fbe01
Hale <crofth@byu.edu>
2023-02-06 23:29:46 -0700

    Changed from Mission 04 to Mission 05.

---
+++ Mission04_crofth/Controllers/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission05_crofth.Controllers
+{
+    public class ViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator ()
+        {
+            return View();
+        }
+    }
+}
--- Mission04_crofth/Program.cs
+++ Mission04_crofth/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mission04_crofth
+namespace Mission05_crofth
 {
     public class Program
     {
